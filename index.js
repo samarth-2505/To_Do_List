5615d2eaf3e9f2727773edfdbdf698fd7b7244eb
Samarth Chaube <18ucc135@lnmiit.ac.in>
2021-06-04 01:56:07 +0530

    First Commit : Create the directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app=express();
+const port=8000;
+
+app.listen(port,function(err){
+  if(err)
+  {
+      console.log(`Error in running the server : ${err}`); //here we used interpolation , we can also write it as console.log('Error in running the server : ', err); 
+      return;
+  }
+  console.log(`Server is up and running on port : ${port}`);
+});
